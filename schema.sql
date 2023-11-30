20430902a53ba83d176dd1dd8a6f95b2a4e8530f
dhiemis <147675163+dhiemis@users.noreply.github.com>
2023-11-30 01:18:52 +0000

    entrega

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE games;
+
+USE games
+
+CREATE TABLE categorias(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE jogos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    competitivo BOOLEAN NOT NULL
+);
+
+CREATE TABLE plataformas(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
